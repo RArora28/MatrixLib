6ceb7168a9877958ee5f3f2cc901723973cb841e
RArora28 <rishabharora1997@gmail.com>
2017-03-11 18:41:43 +0530

    Separted constructor prototypes and definitionsn

--- matrixv1.h
+++ matrixv1.h
@@ -4,22 +4,56 @@
 #define MATRIXV1_INCLUDE
 
 #include <vector>
-#include <type_traits>
 
 template <class ElementType>
 class Matrix {
 public:
 	int row_size, col_size;
 	std::vector< std::vector <ElementType > > mat;
-	Matrix(void) {}
-	Matrix(int r_s, int c_s) : row_size(r_s), col_size(c_s) {
-		mat.resize(row_size);
-		for(int i = 0; i < row_size; i++) {
-			mat[i].resize(col_size);
-		}
-	}
-	Matrix(const Matrix<ElementType> &M) : row_size(M.row_size), col_size(M.col_size), mat(M.mat) {}
+	
+	//constructors
+	Matrix ();
+	Matrix (int r_s, int c_s);
+	Matrix (const Matrix<ElementType> &M);
+
+	//ElementType operator[] (unsigned int ind);
+	//~Matrix();
 };
+
+template <class ElementType>
+Matrix<ElementType>::Matrix() {
+	row_size = col_size = 0;
+}
+
+template <class ElementType>
+Matrix<ElementType>::Matrix(int r_s, int c_s) {
+	row_size = r_s;
+	col_size = c_s;
+	mat.resize(row_size);
+	for(int i = 0; i < row_size; i++) {
+		mat[i].resize(col_size);
+	}
+}
+
+template <class ElementType> 
+Matrix<ElementType>::Matrix(const Matrix<ElementType> &M)  {
+	row_size(M.row_size);
+	col_size(M.col_size);
+	mat.resize(row_size);
+	for(int i = 0; i < row_size; i++) {
+		mat[i].resize(col_size);
+	}
+	for(int i = 0; i < row_size; i++) {
+		for(int j = 0; j < col_size; j++) {
+			mat[i][j] = M.mat[i][j];
+		}
+	}	 
+}
+
+// template <class ElementType>
+// ElementType& Matrix<ElementType>::operator[](int ind) {
+// 	return	mat[ind];s
+// }
 
 template <class ElementType>
 inline Matrix<ElementType> operator+(const Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
@@ -41,7 +75,6 @@ template <class ElementType>
 inline Matrix<ElementType> operator*(const Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
 	//to check if matrix multiplication is dimensionally possible
 	assert(M1.col_size == M2.row_size);
-
 	//alocating O(row*col) additional memory to find the resultant
 	//O(n^3) time complexity
 	Matrix<ElementType> res(M1.row_size, M2.col_size);
@@ -60,8 +93,7 @@ inline void operator+=(Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
 	//to check wheter the matrices have same dimensions
 	assert(M1.row_size == M2.row_size);
 	assert(M1.col_size == M2.col_size);
-	//assert(std::is_integral<ElementType>::value||std::is_floating_point<ElementType>::value||std::izs_integral<ElementType>::value);
-	//alocating O(row*col) additional memory to find the resultant
+	
 	for(int i = 0; i < M1.row_size; i++) {
 		for(int j = 0; j < M1.col_size; j++) {
 			M1.mat[i][j] = M1.mat[i][j] + M2.mat[i][j];
@@ -69,5 +101,22 @@ inline void operator+=(Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
 	}
 	return;
 }
+
+// template <class ElementType>
+// inline void operator*=(Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
+// 	//to check wheter the matrices have same dimensions
+// 	assert(M1.col_size == M2.row_size);
+// 	//alocating O(row*col) additional memory to find the resultant
+
+// 	Matrix<ElementType> 
+// 	for (int i = 0; i < M1.row_size; i++) {
+// 	    for (int j = 0; j < M2.col_size; j++) {
+// 		    res.mat[i][j] = 0;
+//             for (int k = 0; k < M1.col_size; k++)
+//                 res.mat[i][j] += M1.mat[i][k] * M2.mat[k][j];
+//         }
+//     }
+// 	return;
+// }
 
 #endif
