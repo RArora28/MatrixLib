3926d937166e464a8dfce63533427b9e3ecb0aee
RArora28 <rishabharora1997@gmail.com>
2017-03-12 15:42:32 +0530

    added += and *= operations

--- matrixv1.h
+++ matrixv1.h
@@ -1,4 +1,3 @@
-//Matrix class that calculates sum and product of 2 matrices using additional onsite memory
 
 #ifndef MATRIXV1_INCLUDE
 #define MATRIXV1_INCLUDE
@@ -7,64 +6,130 @@
 
 template <class ElementType>
 class Matrix {
-public:
-	int row_size, col_size;
+private:
+	unsigned int row_size, col_size;
 	std::vector< std::vector <ElementType > > mat;
-	
-	//constructors
+
+public:
 	Matrix ();
-	Matrix (int r_s, int c_s);
+	Matrix (const unsigned int r_s, const unsigned int c_s);
 	Matrix (const Matrix<ElementType> &M);
 
-	//ElementType operator[] (unsigned int ind);
-	//~Matrix();
+	//Matrix<ElementType> & operator = (const Matrix<ElementType> &);
+
+	ElementType getVal(const unsigned int row, const unsigned int col);
+	ElementType setVal(const unsigned int row, const unsigned int col, const ElementType value);
+	unsigned int getRowSize();
+	unsigned int getColSize();
+
+	template <class ElType>
+	friend Matrix<ElType> operator + (const Matrix<ElType> &M1, const Matrix<ElType> &M2);
+	template <class ElType>
+	friend Matrix<ElType> operator * (const Matrix<ElType> &M1, const Matrix<ElType> &M2);
+	template <class ElType>
+	friend void operator += (Matrix<ElType> &M1, const Matrix<ElType> &M2);
+	template <class ElType>
+	friend void operator *= (Matrix<ElType> &M1, const Matrix<ElType> &M2);
+		
+	~Matrix();
 };
 
+//constructor declarations
 template <class ElementType>
-Matrix<ElementType>::Matrix() {
+Matrix<ElementType>::Matrix () {
 	row_size = col_size = 0;
 }
 
 template <class ElementType>
-Matrix<ElementType>::Matrix(int r_s, int c_s) {
+Matrix<ElementType>::Matrix (const unsigned int r_s, const unsigned int c_s) {
 	row_size = r_s;
 	col_size = c_s;
 	mat.resize(row_size);
-	for(int i = 0; i < row_size; i++) {
+	for(unsigned int i = 0; i < row_size; i++) {
 		mat[i].resize(col_size);
 	}
 }
 
 template <class ElementType> 
-Matrix<ElementType>::Matrix(const Matrix<ElementType> &M)  {
-	row_size(M.row_size);
-	col_size(M.col_size);
+Matrix<ElementType>::Matrix (const Matrix<ElementType> &M)  {
+	row_size = M.row_size;
+	col_size = M.col_size;
 	mat.resize(row_size);
-	for(int i = 0; i < row_size; i++) {
+	for(unsigned int i = 0; i < row_size; i++) {
 		mat[i].resize(col_size);
 	}
-	for(int i = 0; i < row_size; i++) {
-		for(int j = 0; j < col_size; j++) {
+	for(unsigned int i = 0; i < row_size; i++) {
+		for(unsigned int j = 0; j < col_size; j++) {
 			mat[i][j] = M.mat[i][j];
 		}
 	}	 
 }
 
+
+//destructor declaration
+template <class ElementType> 
+Matrix<ElementType>::~Matrix () {
+	for(unsigned int i = 0; i < row_size; i++) {
+		mat[i].clear();
+	}
+	mat.clear();
+} 
+
+//class operators
 // template <class ElementType>
-// ElementType& Matrix<ElementType>::operator[](int ind) {
-// 	return	mat[ind];s
+// Matrix<ElementType>& Matrix<ElementType>::operator = (const Matrix<ElementType> &M) {
+//     for(unsigned int i = 0; i < row_size; i++) {
+//     	mat[i].clear();
+//     }
+//     mat.clear();
+//     row_size = M.row_size;
+//     col_size = M.col_size;
+//     mat.resize(row_size);
+//     for(unsigned int i = 0; i < row_size; i++) {
+//     	mat[i].resize(col_size);
+//     }
+//     for(unsigned int i = 0; i < row_size; i++) {
+//     	for(unsigned int j = 0; j < col_size; j++) {
+//     		mat[i][j] = M.getVal(i, j);
+//     	}
+//     }
+//     return *this;
 // }
 
+//class functions
 template <class ElementType>
-inline Matrix<ElementType> operator+(const Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
+ElementType Matrix<ElementType>::getVal(const unsigned int row, const unsigned int col) {
+	return mat[row][col];
+}  
+
+template <class ElementType>
+ElementType Matrix<ElementType>::setVal(const unsigned int row, const unsigned int col, const ElementType value) {
+	return mat[row][col] = value;
+}
+
+template <class ElementType>
+unsigned int Matrix<ElementType>::getRowSize() {
+	return row_size;
+}
+
+template <class ElementType>
+unsigned int Matrix<ElementType>::getColSize() {
+	return col_size;
+}
+
+
+//friend functions
+template <class ElementType>
+Matrix<ElementType> operator+ (const Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
 	//to check wheter the matrices have same dimensions
-	assert(M1.row_size == M2.row_size);
-	assert(M1.col_size == M2.col_size);
-	//assert(std::is_integral<ElementType>::value||std::is_floating_point<ElementType>::value||std::izs_integral<ElementType>::value);
+	if(M1.row_size != M2.row_size || M1.col_size != M2.col_size) {
+		std::cerr << "Failed: Invalid matrix dimensions for addition" << std::endl;
+		return M1;
+	}
 	//alocating O(row*col) additional memory to find the resultant
 	Matrix<ElementType> res(M1.row_size, M1.col_size);
-	for(int i = 0; i < M1.row_size; i++) {
-		for(int j = 0; j < M1.col_size; j++) {
+	for(unsigned int i = 0; i < M1.row_size; i++) {
+		for(unsigned int j = 0; j < M1.col_size; j++) {
 			res.mat[i][j] = M1.mat[i][j] + M2.mat[i][j];
 		}
 	}
@@ -72,16 +137,19 @@ inline Matrix<ElementType> operator+(const Matrix<ElementType> &M1, const Matrix
 }
 
 template <class ElementType>
-inline Matrix<ElementType> operator*(const Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
+Matrix<ElementType> operator* (const Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
 	//to check if matrix multiplication is dimensionally possible
-	assert(M1.col_size == M2.row_size);
+	if( M1.col_size != M2.row_size) {
+		std::cerr << "Failed: Invalid matrix dimensions for multiplication" << std::endl;
+		return M1;
+	}
 	//alocating O(row*col) additional memory to find the resultant
 	//O(n^3) time complexity
 	Matrix<ElementType> res(M1.row_size, M2.col_size);
-	for (int i = 0; i < M1.row_size; i++) {
-	    for (int j = 0; j < M2.col_size; j++) {
+	for (unsigned int i = 0; i < M1.row_size; i++) {
+	    for (unsigned int j = 0; j < M2.col_size; j++) {
 		    res.mat[i][j] = 0;
-            for (int k = 0; k < M1.col_size; k++)
+            for (unsigned int k = 0; k < M1.col_size; k++)
                 res.mat[i][j] += M1.mat[i][k] * M2.mat[k][j];
         }
     }
@@ -89,34 +157,55 @@ inline Matrix<ElementType> operator*(const Matrix<ElementType> &M1, const Matrix
 }
 
 template <class ElementType>
-inline void operator+=(Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
+void operator+= (Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
 	//to check wheter the matrices have same dimensions
-	assert(M1.row_size == M2.row_size);
-	assert(M1.col_size == M2.col_size);
-	
-	for(int i = 0; i < M1.row_size; i++) {
-		for(int j = 0; j < M1.col_size; j++) {
+	if(M1.row_size != M2.row_size || M1.col_size != M2.col_size) {
+		std::cerr << "Failed: Invalid matrix dimensions for addition" << std::endl;
+		return;
+	}
+	for(unsigned int i = 0; i < M1.row_size; i++) {
+		for(unsigned int j = 0; j < M1.col_size; j++) {
 			M1.mat[i][j] = M1.mat[i][j] + M2.mat[i][j];
 		}
 	}
 	return;
 }
 
-// template <class ElementType>
-// inline void operator*=(Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
-// 	//to check wheter the matrices have same dimensions
-// 	assert(M1.col_size == M2.row_size);
-// 	//alocating O(row*col) additional memory to find the resultant
+template <class ElementType>
+inline void operator*=(Matrix<ElementType> &M1, const Matrix<ElementType> &M2) {
+	//to check wheter the matrices have same dimensions
+	if(M1.col_size != M2.row_size)
+		return;
+	//alocating O(row*col) additional memory to find the resultant
+	Matrix<ElementType> res(M1.row_size, M2.col_size);
+	for (unsigned int i = 0; i < M1.row_size; i++) {
+	    for (unsigned int j = 0; j < M2.col_size; j++) {
+		    res.mat[i][j] = 0;
+            for (unsigned int k = 0; k < M1.col_size; k++)
+                res.mat[i][j] += M1.mat[i][k] * M2.mat[k][j];
+        }
+    }
+    
+    unsigned int r = M1.row_size, c = M2.col_size;
+    for(unsigned int i = 0; i < M1.row_size; i++) {
+    	M1.mat[i].clear();
+    }
+    M1.mat.clear();
 
-// 	Matrix<ElementType> 
-// 	for (int i = 0; i < M1.row_size; i++) {
-// 	    for (int j = 0; j < M2.col_size; j++) {
-// 		    res.mat[i][j] = 0;
-//             for (int k = 0; k < M1.col_size; k++)
-//                 res.mat[i][j] += M1.mat[i][k] * M2.mat[k][j];
-//         }
-//     }
-// 	return;
-// }
+
+    M1.mat.resize(r);
+    M1.row_size = r;
+    M1.col_size = c;
+    for(unsigned int i = 0; i < r; i++) {
+    	M1.mat[i].resize(c);
+    }
+    
+    for(unsigned int i = 0; i < M1.row_size; i++) {
+    	for(unsigned int j = 0; j < M1.col_size; j++) {
+    		M1.mat[i][j] = res.mat[i][j];
+    	}
+    }
+	return;
+}
 
 #endif
