b4e9bfe0f7fb424d1ee69339bc1364a66687ee06
RArora28 <rishabharora1997@gmail.com>
2017-03-16 02:06:11 +0530

    Example for Matrix multiplication added

---
+++ examples/matrix_mult.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "../matrixv1.h"
+
+int main(int argc, char *argv) {
+
+	Matrix<double> M1(3, 1);
+	Matrix<double> M3(1, 3);
+	M1.setVal(0, 0, 1.2);
+	M1.setVal(1, 0, 2.211);
+	M1.setVal(2, 0, 3.289);
+	M3.setVal(0, 0, 81.2);
+	M3.setVal(0, 1, 25.5211);
+	M3.setVal(0, 2, 31.2589);
+
+	//Binary Multiplication
+	Matrix<double> M4 = M1 * M3;	
+
+	for(int i = 0; i < M4.getRowSize(); i++)
+		for(int j = 0; j < M4.getColSize(); j++)
+			//format : ObjectName.getVal(row, col) returns the value ObjectName.mat[row][col]
+			std::cout << "value at cell " << i << ":" << j << "->" << M4.getVal(i, j) << std::endl;
+
+	std::cout << std::endl;
+
+	//multiple multiplications
+	Matrix<double> M2(1, 1);
+	M2.setVal(0, 0, 5);
+	M4 = M2 * M3 * M1 ;
+
+	for(int i = 0; i < M4.getRowSize(); i++)
+		for(int j = 0; j < M4.getColSize(); j++)
+			//format : ObjectName.getVal(row, col) returns the value ObjectName.mat[row][col]
+			std::cout << "value at cell " << i << ":" << j << "->" << M4.getVal(i, j) << std::endl;
+
+ 	return 0; 
+
+}
