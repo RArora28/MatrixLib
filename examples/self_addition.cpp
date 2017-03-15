af57fe63b4a48affbdfcf31b72dbf27cd7f0af33
RArora28 <rishabharora1997@gmail.com>
2017-03-16 01:58:32 +0530

    example for +=

---
+++ examples/self_addition.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "../matrixv1.h"
+
+int main(int argc, char *argv) {
+
+	Matrix<double> M1(3, 1);
+	Matrix<double> M3(3, 1);
+	M1.setVal(0, 0, 1.2);
+	M1.setVal(1, 0, 2.211);
+	M1.setVal(2, 0, 3.289);
+	M3.setVal(0, 0, 81.2);
+	M3.setVal(1, 0, 25.5211);
+	M3.setVal(2, 0, 31.2589);
+	
+	//binary additions
+	M3 += M1;
+	
+	for(int i = 0; i < M3.getRowSize(); i++)
+		for(int j = 0; j < M3.getColSize(); j++)
+			//format : ObjectName.getVal(row, col) returns the value ObjectName.mat[row][col]
+			std::cout << "value at cell " << i << ":" << j << "->" << M3.getVal(i, j) << std::endl;
+
+ 	return 0; 
+
+}
